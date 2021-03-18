e9a79a77055a434111aafd867c83763d05948e31
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2021-03-18 09:50:29 -0500

    * Contenido Segunda Clase.

--- index.php
+++ index.php
@@ -1,37 +1,70 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form action="index.php" method="get">
-    <input type="text" name="name" id="name">
-    <input type="submit" value="Enviar">
-</form>
-</body>
-</html>
-
 <?php
-$nombre = $_GET['name'] ?? '';
+$nombre = "Diego";
+var_dump($nombre);
+$nombre = 10;
+var_dump($nombre);
+$nombre = TRUE; //True, true
+$nombre = FALSE; //False, false
+var_dump($nombre);
+$nombre = "20"."10";
+var_dump($nombre);
 
-echo 'Hola $nombre </br>';//Comillas Simples
-echo "Hola ".$nombre;   //Comillas Dobles
 
-if ($nombre == "Diego"){
+$nombre = null; //0, -0 => FALSE, 1 => TRUE
+var_dump((bool) $nombre);
+
+$numero = 1.2e3;
+var_dump($numero);
+
+$nombre = "Diego";
+
+$arrayUsuarios = [
+    [
+        "Nombre" => "Diego Ojeda",
+        "Edad" => 28,
+        "Genero" => "Masculino",
+        "Peso" => 55.8,
+        "Administrador" => false
+    ],
+    [
+        "Nombre" => "Juan Valdez",
+        "Edad" => 50,
+        "Genero" => "Masculino",
+        "Peso" => 69.8,
+        "Administrador" => true
+    ],
+];
+
+var_dump($arrayUsuarios);
+
+foreach ($arrayUsuarios as $usuario){
+    echo $usuario['Nombre']."</br>";
+}
+
+// Corto de una sola linea
+/*
+ * Comentario de varias lineas
+ */
+# Tambien sirve para comentar una linea
+//echo 'Hola $nombre </br>';//Comillas Simples
+//echo "Hola ".$nombre;   //Comillas Dobles
+/*if ($nombre == "Diego"){
     echo "Hola ".$nombre;
 }else if($nombre == "Juan"){
     echo "Hola Juan";
 }else{
     echo "Hola Usuario";
-}
+}*/
 ?>
 
-<?php if ($nombre == "Diego"): ?>
-    Esto se mostrará si la expresión es verdadera.
-<?php else: ?>
-    En caso contrario se mostrará esto.
-<?php endif; ?>
+
+<?php
+
+class Usuario {
+
+    // boolean, integer, float, string.
+    // array, object, callable, iterable
+    // resource y null
+    private string $nombre = "Diego";
+
+}
