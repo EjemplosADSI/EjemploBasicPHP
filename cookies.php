d218888e66b7a15408b33f8ad4176a2c24feaf81
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2021-03-24 10:18:35 -0500

    Manejo de Cookies

--- cookies.php
+++ cookies.php
@@ -1,4 +1,11 @@
-<?php session_start(); ?>
+<?php
+    $nombre_cookie = "arrayConductor";
+    $valor_cookie = md5("Diego");
+    $tiempo_cookie = time() + (2*24*60*60); //7 dias, 24 horas, 60 minutos y 60 segundos;
+    $dominio_cookie = "/";
+
+    setcookie($nombre_cookie,$valor_cookie,$tiempo_cookie,"/", false);
+?>
 <!doctype html>
 <html lang="es">
 <head>
@@ -6,11 +13,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pagina Uno</title>
+    <title>Ejemplo Cookies</title>
 </head>
 <body>
     <div>
-        <form action="uno.php" method="post" >
+        <form action="cookies.php" method="post" >
             <input type="text" id="nombre" name="nombre" placeholder="Ingrese sus nombres">
             <input type="text" id="apellido" name="apellido" placeholder="Ingrese sus apellidos">
             <input type="submit" name="Enviar">
@@ -19,7 +26,17 @@
     <div>
         <?php
 
-        if(!empty($_POST['nombre']) && !empty($_POST['apellido'])){ //Verificamos si llega un nombre
+        if(!empty($_POST['nombre'])){
+
+            if(md5($_POST['nombre']) == $_COOKIE['arrayConductor'] ){
+                echo "Si eres el usuario";
+            }else{
+                echo "Lo lamento eres un impostor";
+            }
+
+        }
+
+/*        if(!empty($_POST['nombre']) && !empty($_POST['apellido'])){ //Verificamos si llega un nombre
             $arrUser = [
                 'Nombres' => $_POST['nombre'],
                 'Apellidos' => $_POST['apellido']
@@ -28,10 +45,11 @@
             var_dump($_SESSION['ArrUsers']);
         }
 
-        $arrayConductor = !empty($_SESSION['arrayConductor']) ? $_SESSION['arrayConductor'] : '';
+        $arrayConductor = !empty($_SESSION['arrayConductor']) ? $_SESSION['arrayConductor'] : '';*/
 
         ?>
-        <a href="dos.php">Limpar Nombres</a>
+        <a href="#">Limpar Nombres</a>
     </div>
+
 </body>
 </html>
